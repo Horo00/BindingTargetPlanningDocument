763169f817513145ac00f0c3739f83c3e0e110b1
Horo00 <em7c8ctakahiro@yahoo.co.jp>
2020-11-02 11:57:14 +0900

    second

---
+++ app/src/main/java/work/horo/bindingtargetplanningdocument/MainActivity.java
@@ -0,0 +1,14 @@
+package work.horo.bindingtargetplanningdocument;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
